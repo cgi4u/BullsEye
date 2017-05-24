e06c220e901d9ad2d77248da3a3ab87665d907dc
최광익 <choegwang-ig@choegwang-ig-ui-MacBook-Pro.local>
2017-05-24 14:24:42 +0900

    Verion 9.1: Relpace Functions

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        startNewRound()
+        startNewGame()
         updateLabels()
     }
 
